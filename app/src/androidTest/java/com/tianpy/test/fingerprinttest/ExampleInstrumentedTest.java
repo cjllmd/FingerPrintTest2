7e399bdc14b78e4911a30ba2ad8cbc1c31268cbf
tianpy <13400717@qq.com>
2018-09-06 16:46:23 +0800

    init

--- app/src/androidTest/java/com/tianpy/test/fingerprinttest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tianpy/test/fingerprinttest/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-            //sssss
+            //sssss1
         assertEquals("com.tianpy.test.fingerprinttest", appContext.getPackageName());
     }
 }
